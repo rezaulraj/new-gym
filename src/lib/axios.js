70942be769d2bbff54cf59bcc45b74c460b432ec
rezaul-karim <rezaul.coderpro@gmail.com>
2025-03-17 20:01:35 +0600

    third commit

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_BASE_URL ||
-    "https://67d6992d286fdac89bc262e0.mockapi.io/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default axiosInstance;
+
 // https://67d6992d286fdac89bc262e0.mockapi.io/api/gym/products
 // https://67d6992d286fdac89bc262e0.mockapi.io/api/gym/category
